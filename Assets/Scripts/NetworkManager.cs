0d6e1a8ee7c5d778be1436cff98d5d6d62e98486
Johnny Stark <nbasakuragi@gmail.com>
2014-10-24 15:43:49 +0100

    networkmanager improvements

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -9,6 +9,33 @@ public class NetworkManager : MonoBehaviour
     string GameName = "Galaga_Networking";
     private bool _refreshing;
 
+    private string _proxyIp = "";
+    private HostData[] hostData;
+
+    // Awake is called when the script instance is being loaded (Since v1.0)
+    void Awake()
+    {
+        StartServer();
+    }
+
+    void StartServer()
+    {
+        if (hostData != null && hostData.Length > 0)
+            foreach (var data in hostData)
+            {
+                Network.Connect(data);
+            }
+        else
+        {
+            MasterServer.ipAddress = "127.0.0.1";
+            var useNat = !Network.HavePublicAddress();
+
+            Network.InitializeServer(1, 25001, useNat);
+            MasterServer.RegisterHost(GameName, "Galaga", "Multiplayer Game");
+            RefreshHost();
+
+        }
+    }
 
     void OnServerInitialized()
     {
@@ -24,38 +51,115 @@ public class NetworkManager : MonoBehaviour
             Debug.Log("Registered");
         }
     }
+    // Called on the client when a connection attempt fails for some reason (Since v2.0)
+    void OnFailedToConnect(NetworkConnectionError error)
+    {
+        switch (error)
+        {
+            case NetworkConnectionError.NoError:
+                break;
+            case NetworkConnectionError.RSAPublicKeyMismatch:
+                break;
+            case NetworkConnectionError.InvalidPassword:
+                break;
+            case NetworkConnectionError.ConnectionFailed:
+                break;
+            case NetworkConnectionError.TooManyConnectedPlayers:
+                break;
+            case NetworkConnectionError.ConnectionBanned:
+                break;
+            case NetworkConnectionError.AlreadyConnectedToServer:
+                break;
+            case NetworkConnectionError.AlreadyConnectedToAnotherServer:
+                break;
+            case NetworkConnectionError.CreateSocketOrThreadFailure:
+                break;
+            case NetworkConnectionError.IncorrectParameters:
+                break;
+            case NetworkConnectionError.EmptyConnectTarget:
+                break;
+            case NetworkConnectionError.InternalDirectConnectFailed:
+                break;
+            case NetworkConnectionError.NATTargetNotConnected:
+                break;
+            case NetworkConnectionError.NATTargetConnectionLost:
+                break;
+            case NetworkConnectionError.NATPunchthroughFailed:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("error");
+        }
+
+
+    }
+
+    // Called on clients or servers when there is a problem connecting to the MasterServer (Since v2.0)
+    void OnFailedToConnectToMasterServer(NetworkConnectionError info)
+    {
+        switch (info)
+        {
+            case NetworkConnectionError.NoError:
+                break;
+            case NetworkConnectionError.RSAPublicKeyMismatch:
+                break;
+            case NetworkConnectionError.InvalidPassword:
+                break;
+            case NetworkConnectionError.ConnectionFailed:
+                break;
+            case NetworkConnectionError.TooManyConnectedPlayers:
+                break;
+            case NetworkConnectionError.ConnectionBanned:
+                break;
+            case NetworkConnectionError.AlreadyConnectedToServer:
+                break;
+            case NetworkConnectionError.AlreadyConnectedToAnotherServer:
+                break;
+            case NetworkConnectionError.CreateSocketOrThreadFailure:
+                break;
+            case NetworkConnectionError.IncorrectParameters:
+                break;
+            case NetworkConnectionError.EmptyConnectTarget:
+                break;
+            case NetworkConnectionError.InternalDirectConnectFailed:
+                break;
+            case NetworkConnectionError.NATTargetNotConnected:
+                break;
+            case NetworkConnectionError.NATTargetConnectionLost:
+                break;
+            case NetworkConnectionError.NATPunchthroughFailed:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("info");
+        }
+    }
+
 
     void RefreshHost()
     {
         Debug.Log("Refresh Host");
         MasterServer.RequestHostList(GameName);
         _refreshing = true;
-        Debug.Log(MasterServer.PollHostList().Length);
 
     }
 
     void OnGUI()
     {
-        var startServer = GUI.Button(new Rect(btnX, btnY, btnW, btnH), "Start Server");
-        var refreshHost = GUI.Button(new Rect(btnX, btnY * 3.2f, btnW, btnH), "Refresh Host");
+        //var startServer = GUI.Button(new Rect(btnX, btnY, btnW, btnH), "Start Server");
+        //var refreshHost = GUI.Button(new Rect(btnX, btnY * 3.2f, btnW, btnH), "Refresh Host");
 
-        if (startServer)
-        {
-            Debug.Log("Server started");
-            var useNat = !Network.HavePublicAddress();
-            try
-            {
-                Network.InitializeServer(1, 25001, useNat);
-                MasterServer.RegisterHost(GameName, "Galaga", "Multiplayer Game");
-            }
-            catch (Exception e)
-            {
-                Debug.Log(e);
-            }
-        }
+        //if (startServer)
+        //{
+        //    Debug.Log("Server started");
+        //    try
+        //    {
+        //        MasterServer.RegisterHost(GameName, "Galaga", "Multiplayer Game");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Debug.Log(e);
+        //    }
+        //}
 
-        if (refreshHost) 
-            RefreshHost();
 
     }
 
@@ -71,6 +175,10 @@ public class NetworkManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!_refreshing || MasterServer.PollHostList().Length <= 0) return;
 
+        _refreshing = false;
+        hostData = MasterServer.PollHostList();
+        Debug.Log(hostData.Length);
     }
 }
