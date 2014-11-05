0929e5ed24373fb7de975974547e4ed4a6e25e5f
nbasakuragi <nbasakuragi@gmail.com>
2014-11-05 23:39:00 +0000

    started implementing enemies

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WeaponController : MonoBehaviour
+    {
+        public GameObject Shot;
+        public Transform ShotSpawn;
+        public float FireRate;
+        public float Delay;
+
+        void Start()
+        {
+            InvokeRepeating("Fire", Delay, FireRate);
+        }
+
+        void Fire()
+        {
+            GameObjectController.Instantiate(Shot, ShotSpawn.position, ShotSpawn.rotation);
+        }
+    }
+}
